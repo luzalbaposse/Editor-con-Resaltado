78775c85cff8f3a3ca486885e0b561ac7081b455
Luz Alba Posse <luzalbaposse@gmail.com>
2024-06-17 10:45:02 -0300

    Fin. TP

--- tp-codigo/EditorResaltado.cpp
+++ tp-codigo/EditorResaltado.cpp
@@ -7,164 +7,189 @@
 
 using namespace std;
 
-EditorResaltado::EditorResaltado(){ comentarios = vector<Comentario>(); palabras = vector<string>(); comentarios_por_palabra = vector<set<id_comm>>(); comentarios_texto = map<id_comm, string>(); }
+EditorResaltado::EditorResaltado() {
+    comentarios = vector<Comentario>(); // O(1)
+    palabras = vector<string>(); // O(1)
+    comentarios_por_palabra = vector<set<id_comm>>(); // O(1)
+    comentarios_texto = map<id_comm, string>(); // O(1)
+}
 
-unsigned EditorResaltado::longitud() const { /* O(1) */
-    cout << "Longitud: " << palabras.size() << endl;
-	return palabras.size();
+unsigned EditorResaltado::longitud() const { 
+    /* O(1) porque simplemente devuelve el tamaño del vector palabras, que es una operación constante. */
+    return palabras.size(); // O(1)
 }
 
 void EditorResaltado::cargar_texto(const string& txt, const string& comments) {
-    ifstream archivo_texto_stream(txt);
+    /*
+    O(P + C) porque leer el texto y los comentarios y actualizar las estructuras de datos correspondientes implica recorrer cada palabra y comentario una vez.
+    */
+    
+    ifstream archivo_texto_stream(txt); // O(1)
+    ifstream archivo_comentarios_stream(comments); // O(1)
 
-    ifstream archivo_comentarios_stream(comments);
-
-    palabras.clear();
-    comentarios.clear();
-    comentarios_texto.clear();
-    comentarios_por_palabra.clear();
+    palabras.clear(); // O(P)
+    comentarios.clear(); // O(C)
+    comentarios_texto.clear(); // O(C)
+    comentarios_por_palabra.clear(); // O(P)
 
     string palabra;
-    while (archivo_texto_stream >> palabra) {
-        palabras.push_back(palabra);
-        comentarios_por_palabra.emplace_back();
+    while (archivo_texto_stream >> palabra) { // O(P)
+        palabras.push_back(palabra); // O(1)
+        comentarios_por_palabra.emplace_back(); // O(1)
     }
 
     string linea_comentario;
     id_comm id = 0;
-    while (getline(archivo_comentarios_stream, linea_comentario)) {
-        istringstream ss(linea_comentario);
+    while (getline(archivo_comentarios_stream, linea_comentario)) { // O(C)
+        istringstream ss(linea_comentario); // O(1)
         unsigned desde, hasta;
-        ss >> desde >> hasta;
+        ss >> desde >> hasta; // O(1)
         string comentario;
-        getline(ss, comentario);
+        getline(ss, comentario); // O(1)
 
-        size_t first_non_space = comentario.find_first_not_of(" ");
+        size_t first_non_space = comentario.find_first_not_of(" "); // O(1)
         if (first_non_space != string::npos) {
-            comentario = comentario.substr(first_non_space);
+            comentario = comentario.substr(first_non_space); // O(1)
         }
 
-        Comentario c = Comentario(comentario, make_pair(desde, hasta), id++);
-        comentarios.push_back(c);
-        comentarios_texto[c.id] = comentario;
+        Comentario c = Comentario(comentario, make_pair(desde, hasta), id++); // O(1)
+        comentarios.push_back(c); // O(1)
+        comentarios_texto[c.id] = comentario; // O(log C)
         for (unsigned i = desde; i < hasta; ++i) { 
-            comentarios_por_palabra[i].insert(c.id);
+            comentarios_por_palabra[i].insert(c.id); // O(log C)
         }
     }
 }
 
-
 const string& EditorResaltado::palabra_en(unsigned pos) const {
-/*O(1)*/
-	return palabras[pos];
+    /*
+    O(1) porque simplemente devuelve la palabra en la posición pos del vector palabras, que es una operación constante.
+    */
+    return palabras[pos]; // O(1)
 }
 
 const string& EditorResaltado::texto_comentario(id_comm id) const {
-
-    auto it = comentarios_texto.find(id); 
-    return it->second;
+    /*
+    O(log C) porque buscar un elemento en un mapa (std::map) tiene una complejidad logarítmica.
+    */
+    auto it = comentarios_texto.find(id); // O(log C)
+    return it->second; // O(1)
 }
 
-
-const set<id_comm> & EditorResaltado::comentarios_palabra(unsigned pos) const {
-	//* TODO: implementar y justificar complejidad
-	return comentarios_por_palabra[pos];
+const set<id_comm>& EditorResaltado::comentarios_palabra(unsigned pos) const {
+    /*
+    O(1) porque devuelve el conjunto de comentarios en la posición pos del vector comentarios_por_palabra.
+    */
+    return comentarios_por_palabra[pos]; // O(1)
 }
 
 void EditorResaltado::insertar_palabra(const string& palabra, unsigned pos) {
-	//* TODO: implementar y justificar complejidad
-    palabras.insert(palabras.begin() + pos, palabra);
-    comentarios_por_palabra.insert(comentarios_por_palabra.begin() + pos, set<id_comm>());
+    /* 
+    O(P + C) porque insertar una palabra en palabras y un nuevo conjunto en comentarios_por_palabra requiere mover elementos, cada uno con complejidad O(P). Después, ajustar los rangos de los comentarios y moverlos implica iterar sobre los comentarios, lo cual tiene una complejidad O(C), resultando en O(P + C).
+    */
+    palabras.insert(palabras.begin() + pos, palabra); // O(P)
+    comentarios_por_palabra.insert(comentarios_por_palabra.begin() + pos, set<id_comm>()); // O(P)
 
-    for (unsigned i = 0; i < pos; ++i) {
-        for (const auto& id : comentarios_por_palabra[i]) {
-            comentarios_por_palabra[pos].insert(id);
+    for (auto& comentario : comentarios) { // O(C)
+        if (comentario.rango.first <= pos && comentario.rango.second >= pos) { // O(1)
+            comentario.rango.second++; // O(1)
+            comentarios_por_palabra[pos].insert(comentario.id); // O(log C)
+        } else if (comentario.rango.first > pos) { // O(1)
+            comentario.rango.first++; // O(1)
+            comentario.rango.second++; // O(1)
         }
     }
 
-    for (unsigned i = pos + 1; i < comentarios_por_palabra.size(); ++i) {
-        for (const auto& id : comentarios_por_palabra[i]) {
-            comentarios_por_palabra[i - 1].insert(id);
-        }
+    for (unsigned i = pos + 1; i < comentarios_por_palabra.size(); ++i) { // O(P)
+        comentarios_por_palabra[i].insert(comentarios_por_palabra[i - 1].begin(), comentarios_por_palabra[i - 1].end()); // O(log C)
     }
 }
+
 void EditorResaltado::borrar_palabra(unsigned pos) {
-    palabras.erase(palabras.begin() + pos);
-    set<id_comm> comentarios_eliminados = comentarios_por_palabra[pos];
-    comentarios_por_palabra.erase(comentarios_por_palabra.begin() + pos);
+    /*
+    O(P + C) porque eliminar una palabra de palabras y un conjunto de comentarios_por_palabra es O(P), dada la necesidad de mover elementos.
 
-    for (auto id : comentarios_eliminados) {
-        cerr << id << " ";
+    Crear un conjunto comentarios_activos para verificar la presencia de comentarios restantes tiene una complejidad combinada de O(P + C), ya que implica iterar sobre todas las posiciones y comentarios.
+    
+    Chequear y eliminar comentarios vacíos tiene una complejidad O(C), porque implica iterar sobre los comentarios eliminados y realizar búsquedas en un conjunto, lo que es una operación constante.
+    */
+    palabras.erase(palabras.begin() + pos); // O(P)
+    set<id_comm> comentarios_eliminados = comentarios_por_palabra[pos]; // O(1)
+    comentarios_por_palabra.erase(comentarios_por_palabra.begin() + pos); // O(P)
+
+    set<id_comm> comentarios_activos;
+    for (const auto& comentarios_set : comentarios_por_palabra) { // O(P)
+        comentarios_activos.insert(comentarios_set.begin(), comentarios_set.end()); // O(log C)
     }
-    cerr << endl;
 
-    for (auto id : comentarios_eliminados) {
-        bool comentario_vacio = true;
-        for (unsigned i = 0; i < comentarios_por_palabra.size(); ++i) {
-            if (comentarios_por_palabra[i].find(id) != comentarios_por_palabra[i].end()) {
-                comentario_vacio = false;
-                break;
-            }
-        }
-        cerr << "Comentario " << id << " está vacío: " << comentario_vacio << endl;
-        if (comentario_vacio) {
-            comentarios_texto.erase(id);
-            auto it = std::remove_if(comentarios.begin(), comentarios.end(), [id](const Comentario& c) { return c.id == id; });
+    for (auto id : comentarios_eliminados) { // O(C)
+        if (comentarios_activos.find(id) == comentarios_activos.end()) { // O(log C)
+            comentarios_texto.erase(id); // O(log C)
+            auto it = std::remove_if(comentarios.begin(), comentarios.end(), [id](const Comentario& c) { return c.id == id; }); // O(C)
             if (it != comentarios.end()) {
-                comentarios.erase(it, comentarios.end());
+                comentarios.erase(it, comentarios.end()); // O(C)
             }
         }
     }
 }
 
 id_comm EditorResaltado::comentar(const string& texto, unsigned pos_desde, unsigned pos_hasta) {
-    Comentario c = Comentario(texto, make_pair(pos_desde, pos_hasta), comentarios.size());
+    /*
+    O(C) porque añade un comentario y actualiza las estructuras de datos correspondientes, lo cual implica iterar sobre cada palabra afectada por el comentario.
+    */
+    Comentario c = Comentario(texto, make_pair(pos_desde, pos_hasta), comentarios.size()); // O(1)
+    comentarios.push_back(c); // O(1)
+    comentarios_texto[comentarios.size() - 1] = texto; // O(log C)
 
-    comentarios.push_back(c);
-    comentarios_texto[comentarios.size() - 1] = texto;
-
-
-    for (unsigned i = pos_desde; i < pos_hasta; ++i) {
-        comentarios_por_palabra[i].insert(comentarios.size() - 1);
+    for (unsigned i = pos_desde; i < pos_hasta; ++i) { // O(C)
+        comentarios_por_palabra[i].insert(comentarios.size() - 1); // O(log C)
     }
 
-
-    return comentarios.size() - 1;
+    return comentarios.size() - 1; // O(1)
 }
 
-
 void EditorResaltado::resolver_comentario(id_comm id) {
-	//* TODO: implementar y justificar complejidad
-    for (auto& comentarios_set : comentarios_por_palabra) {
-        comentarios_set.erase(id);
+    /*
+    O(log C + R_i log M) porque eliminamos el comentario de cada conjunto en comentarios_por_palabra solo si contiene el comentario (O(R_i log C)). Eliminar el comentario de comentarios_texto es O(log C), y buscar y eliminar el comentario de comentarios se realiza en O(log M).
+    */
+    for (auto& comentarios_set : comentarios_por_palabra) { // O(P)
+        if (comentarios_set.find(id) != comentarios_set.end()) {
+            comentarios_set.erase(id); // O(log C)
+        }
     }
-    comentarios_texto.erase(id);
-    auto it = std::find_if(comentarios.begin(), comentarios.end(), [id](const Comentario& c) { return c.id == id; });
+
+    comentarios_texto.erase(id); // O(log C)
+
+    auto it = std::find_if(comentarios.begin(), comentarios.end(), [id](const Comentario& c) { return c.id == id; }); // O(C)
     if (it != comentarios.end()) {
-        comentarios.erase(it);
+        comentarios.erase(it); // O(C)
     }
 }
 
 unsigned EditorResaltado::cantidad_comentarios() const {
-	//* TODO: implementar y justificar complejidad
-    cout << "Cantidad de comentarios: " << comentarios.size() << endl;
-	return comentarios.size();
+    /*
+    O(1) porque devuelve una constante.
+    */
+    return comentarios.size(); // O(1)
 }
 
 EditorResaltado EditorResaltado::con_texto(const string& texto) {
+    /*
+    O(P) porque la función itera sobre cada carácter de la cadena una vez para dividir el texto en palabras y almacenarlas en las estructuras de datos correspondientes.
+    */
     EditorResaltado editor;
 
-	int i = 0;
-	while (i < texto.size()){
-		string palabra = "";
-		while(i < texto.size() && texto[i] != ' '){
-			palabra += texto[i];
-			i++;
-		}
-		editor.palabras.push_back(palabra);
-		editor.comentarios_por_palabra.emplace_back();
-		i++;
-	}
-	
-    return editor;
+    int i = 0;
+    while (i < texto.size()) { // O(P)
+        string palabra = "";
+        while (i < texto.size() && texto[i] != ' ') { // O(L) con L = longitud de cada palabra
+            palabra += texto[i]; // O(1)
+            i++; // O(1)
+        }
+        editor.palabras.push_back(palabra); // O(1)
+        editor.comentarios_por_palabra.emplace_back(); // O(1)
+        i++; // O(1)
+    }
+    
+    return editor; // O(1)
 }
--- tp-codigo/EditorResaltado.h
+++ tp-codigo/EditorResaltado.h
@@ -15,79 +15,55 @@ class EditorResaltado {
 	public:
 		// Constructores
 		EditorResaltado();
-		// Construye un editor vacío
-		// Método estático auxiliar para construir editores de forma conveniente
+		
 		static EditorResaltado con_texto(const string& texto);
-		// Devuelve un editor con las palabras de texto insertadas en su orden original
 
-		// Observadores
 		unsigned longitud() const;
-		// Devuelve la cantidad de palabras del texto
+
 		const string& palabra_en(unsigned pos) const;
-		// devuelve la palabra en esa posición
+
 		const string& texto_comentario(id_comm id) const;
-		// devuelve el texto del comentario id
+
 		const set<id_comm> & comentarios_palabra(unsigned pos) const;
-		// devuelve los comentarios de la palabra en esa posición
 
 		// Modificadores
 	  	void cargar_texto(const string& archivo_texto, const string& archivo_comentarios);
-		// Borra el contenido viejo, carga el texto de archivo_texto y le palica los comentarios de archivo_comentarios
+
 		void insertar_palabra(const string& palabra, unsigned pos);
-		// Inserta palabra en la posición pos. Si pos = longitud se inserta al final Si pos era previamnete afectada por uno o más comentarios, los mismos se expanderán para contener a la palabra insertada también
+
 		void borrar_palabra(unsigned pos);
-		// Post: elimina la palabra ubicada en la posición pos. Elimina cualquier comentario cuya única posición afectada fuera pos
+
 		id_comm comentar(const string& comentario, unsigned desde, unsigned hasta);
-		// Devuelve el ID de un nuevo comentario que aplica a todas las palabras entre desde y hasta (sin incluir esta última posición). Nota: Los IDs deben asignarse de manera creciente empezando por 1. No se reutilizan IDs de comentarios eliminados.
 
 		void resolver_comentario(id_comm id);
-		// Se elimina id de todas las piosiciones afectadas por este
+
 		// Otras operaciones
 		unsigned cantidad_comentarios() const;
-		// Devuelve la cantidad de comentarios que hay en el texto
 
 	private:
-		//* TODO: Definir la estructura y describir claramente el invariante de representación de la misma.
-		//? CONSULTAR
-		/*
-		Invariante de representación:
-		Rep(e: estr) ≡
-		(∀i ∈ [0, e.longitud()), e.comentarios_por_palabra[i] ≠ ∅) ∧
-		(∀id ∈ claves(e.comentarios_texto), ∃c ∈ e.comentarios → c.id == id) ∧
-		(e.longitud() == |e.comentarios_por_palabra|) ∧
-		(∀id1, id2 ∈ claves(e.comentarios_texto), id1 ≠ id2) ∧ e.longitud() >= 0 ∧ e.cant_comentarios >= 0 
+		/*		
+		Rep(e: estr) ≡ (∀i ∈ [0, e.longitud()), e.comentarios_por_palabra[i] ≠ ∅) ∧ (∀id ∈ claves(e.comentarios_texto), ∃c ∈ e.comentarios → c.id == id) ∧ (e.longitud() == |e.comentarios_por_palabra|) ∧ (∀id1, id2 ∈ claves(e.comentarios_texto), id1 ≠ id2) ∧ e.longitud() >= 0 ∧ e.cant_comentarios >= 0 
 
-		1. ∀i ∈ [0, e.longitud()), e.comentarios_por_palabra[i] ≠ ∅:
-		Para cada posición i en el vector de palabras, existe un conjunto de IDs de comentarios.
-
-		2. ∀id ∈ claves(e.comentarios_texto), ∃c ∈ e.comentarios → c.id == id:
-		Para cada ID de comentario en comentarios_texto, existe un comentario correspondiente en comentarios con el mismo ID.
-
-		3. e.longitud() == |e.comentarios_por_palabra|:
-		La cantidad de elementos en el vector de palabras es igual a la cantidad de elementos en el vector de conjuntos de IDs de comentarios.
-
-		4. ∀id1, id2 ∈ claves(e.comentarios_texto), id1 ≠ id2:
-		Los IDs de los comentarios en comentarios_texto son únicos.
+		Para cada posición i en el vector de palabras, existe un conjunto de IDs de comentarios. Para cada ID de comentario en comentarios_texto, existe un comentario correspondiente en comentarios con el mismo ID. La cantidad de elementos en el vector de palabras es igual a la cantidad de elementos en el vector de conjuntos de IDs de comentarios. Los IDs de los comentarios en comentarios_texto son únicos.
 		*/
 
-		//* Estructura de comentarios
 		struct Comentario {
 			string texto;
 			pair<unsigned, unsigned> rango;  // Tupla de rango del comentario
 			id_comm id;                      // ID del comentario
-			bool resuelto;                   // Indica si el comentario fue resuelto
 
 			Comentario(string texto, pair<unsigned, unsigned> rango, id_comm id)
-				: texto(texto), rango(rango), id(id), resuelto(false) {}
+				: texto(texto), rango(rango), id(id) {}
 		};
 		
 		vector<Comentario> comentarios;
 				
 		vector<string> palabras;
-		// Mapea la posición de cada palabra a los IDs de los comentarios
+
 		vector<set<id_comm>> comentarios_por_palabra;
-		// Mapea los IDs de comentarios a su texto
+
 		map<id_comm, string> comentarios_texto;
+		// Este map puede resultar medio redundante, pero el objetivo es reducir la complejidad de la operación texto_comentario(id_comm id) a O(1) en lugar de O(log C) si se recorriera el vector de comentarios buscando el comentario con el ID correspondiente. Lo mismo con las demás funciones.
 		
 
 		};
